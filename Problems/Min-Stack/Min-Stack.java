564abb09a12215b9c1e356d4cfe282a77a215c63
aflr <143635386+aflr@users.noreply.github.com>
2023-09-04 23:15:19 +0200

    Create Min-Stack.java

---
+++ Problems/Min-Stack/Min-Stack.java
@@ -0,0 +1,33 @@
+class MinStack {
+
+    private List<Integer> stack;
+
+    public MinStack() {
+        stack = new ArrayList<Integer>();
+    }
+    
+    public void push(int val) {
+        stack.add(val);
+    }
+    
+    public void pop() {
+        stack.remove(stack.size() - 1);
+    }
+    
+    public int top() {
+        return stack.get(stack.size() - 1);
+    }
+    
+    public int getMin() {
+        return Collections.min(stack);
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
