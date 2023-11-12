755459a08a8746da391e0dabb2fdb226e9ed8e31
Alberto Florido <143635386+aflr@users.noreply.github.com>
2023-11-12 02:47:08 +0100

    Create Timeout-Cancellation.js

---
+++ Problems/Timeout-Cancellation/Timeout-Cancellation.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    let c = setTimeout(() => fn(...args), t);
+    return () => clearTimeout(c);
+};
