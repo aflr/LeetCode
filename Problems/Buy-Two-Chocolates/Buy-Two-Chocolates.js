1f96269ecc14e76edd7f8a937e0c52eedd7d9139
Alberto Florido <143635386+aflr@users.noreply.github.com>
2023-12-20 22:09:21 +0100

    Create Buy-Two-Chocolates.js

---
+++ Problems/Buy-Two-Chocolates/Buy-Two-Chocolates.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} prices
+ * @param {number} money
+ * @return {number}
+ */
+var buyChoco = function(prices, money) {
+    prices.sort((a, b) => a - b);
+    return prices[0] + prices[1] <= money ? money - prices[0] - prices[1] : money;
+};
