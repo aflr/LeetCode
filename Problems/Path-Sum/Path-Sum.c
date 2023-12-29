8efd51764379f087a05189cff8b104d9e4f675c0
Alberto Florido <143635386+aflr@users.noreply.github.com>
2023-12-29 02:22:01 +0100

    Create Path-Sum.c

---
+++ Problems/Path-Sum/Path-Sum.c
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool hasPathSum(struct TreeNode* root, int targetSum) {
+    if (!root)
+        return false;
+    if (!root->left && !root->right)
+        return targetSum == root->val;
+    return hasPathSum(root->left, targetSum - root->val) ||
+           hasPathSum(root->right, targetSum - root->val);
+}
