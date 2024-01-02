db6a47e1416c2a2b9e27722a2a4b1ac00651c8bc
Alberto Florido <143635386+aflr@users.noreply.github.com>
2024-01-02 01:01:59 +0100

    Create Reverse-Bits.cpp

---
+++ Problems/Reverse-Bits/Reverse-Bits.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t r = 0;
+        for (int b = 32 - 1; b >= 0; b--)
+            r |= (n >> b & 1) << (32 - 1 - b);
+        return r;
+    }
+};
